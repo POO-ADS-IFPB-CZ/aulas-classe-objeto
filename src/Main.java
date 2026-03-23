be4e81792f698192571d4555bdf126aa81fdb038
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-03-23 09:14:37 -0300

    Exemplos de manipulação de datas

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ import modelo.Produto;
 
 void main() {
 
-    Produto produto = new Produto(1, "Arroz",
-            4.5, LocalDate.of(2026,3,3),
-            true);
-    Produto produto1 = new Produto();
-    Produto produto2 = new Produto(2);
+    DateTimeFormatter formatter = DateTimeFormatter
+            .ofPattern("dd/MM/yyyy");
 
-    System.out.println(Produto.getQuantidade());
+    //Saída
+    System.out.println(formatter.format(LocalDate.now()));
 
+    String data = IO.readln("Informe a data desejada:");
+    LocalDate minhaData = LocalDate.parse(data, formatter);
 
 }
